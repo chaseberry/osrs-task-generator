35c095d1d067c0aadfed86d55cb64202cd376919
Chase Berry <laxperson808@gmail.com>
2026-05-21 19:13:16 -0400

    almost ready to test

--- src/main/kotlin/com/chase/OsrsTaskGenerator.kt
+++ src/main/kotlin/com/chase/OsrsTaskGenerator.kt
@@ -1,5 +1,8 @@
 package com.chase
 
+import com.chase.cli.Cli
+import com.chase.providers.sources.InMemoryCustomTaskProvider
+import com.chase.providers.sources.InMemoryItemProvider
 import com.chase.providers.sources.InMemoryItemSourceProvider
 import com.chase.utilities.readFile
 import kotlinx.coroutines.Dispatchers
@@ -25,8 +28,12 @@ class OsrsTaskGenerator(
         }
     }
 
-    private fun runCli(runConfiguration: RunConfiguration) {
-
+    private suspend fun runCli(runConfiguration: RunConfiguration) = with(runConfiguration.dataSource) {
+        Cli(
+            itemSourceProvider(),
+            itemProvider(),
+            customTaskProvider()
+        ).run()
     }
 
     private suspend fun parseConfiguration(): RunConfiguration {
@@ -43,15 +50,18 @@ class OsrsTaskGenerator(
 
     private fun parseConfigurationJson(json: String): RunConfiguration = Json.decodeFromString(json)
 
-    private suspend fun setupProviders(runConfiguration: RunConfiguration) {
-        when (val s = runConfiguration.dataSource) {
-            is RunConfiguration.DataSource.InMemory -> {
-                InMemoryItemSourceProvider(
-                    preseed(s.itemSourcePreseedFile)
-                )
-            }
-        }
+    private suspend fun RunConfiguration.DataSource.itemSourceProvider() = when (this) {
+        is RunConfiguration.DataSource.InMemory -> InMemoryItemSourceProvider(preseed(itemSourcePreseedFile))
+    }
+
+    private suspend fun RunConfiguration.DataSource.itemProvider() = when (this) {
+        is RunConfiguration.DataSource.InMemory -> InMemoryItemProvider(preseed(itemPreseedFile))
+    }
+
+    private suspend fun RunConfiguration.DataSource.customTaskProvider() = when (this) {
+        is RunConfiguration.DataSource.InMemory -> InMemoryCustomTaskProvider(preseed(customTaskPreseedFile))
     }
 
     suspend inline fun <reified T> preseed(path: String): List<T> = Json.decodeFromString(readFile(path))
+
 }
