cf96c4448a173b5af1f720785aa7c99dc2ba8f7b
Chase Berry <laxperson808@gmail.com>
2026-05-20 22:45:22 -0400

    Core work on cli

--- src/main/kotlin/com/chase/cli/Cli.kt
+++ src/main/kotlin/com/chase/cli/Cli.kt
@@ -2,6 +2,8 @@ package com.chase.cli
 
 import com.chase.providers.ItemProvider
 import com.chase.providers.ItemSourceProvider
+import com.chase.utilities.ItemBuilder
+import com.chase.utilities.ItemSourceBuilder
 import com.chase.utilities.param
 import com.chase.utilities.runs
 
@@ -20,6 +22,38 @@ class Cli(
 
                 }
             }
+        },
+        "add" runs {
+            subCommand {
+                "items" runs {
+                    ItemBuilder().getItems().forEach {
+                        try {
+                            itemProvider.add(it)
+                            println("Added $it")
+                        } catch (e: IllegalArgumentException) {
+                            println("Failed to add $it")
+                            println(e.message)
+                        }
+                    }
+                }
+                "itemsources" runs {
+                    ItemSourceBuilder().getItems().forEach {
+                        try {
+                            itemSourceProvider.add(it)
+                            println("Added $it")
+                        } catch (e: IllegalArgumentException) {
+                            println("Failed to add $it")
+                            println(e.message)
+                        }
+                    }
+                }
+            }
+        },
+        "export" runs {
+            TODO("export providers to save file")
+        },
+        "generate" runs {
+
         }
     )
 
